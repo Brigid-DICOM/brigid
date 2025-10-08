48b1f034c93b83553078372d2975462aafe208c2
chinlinlee <a5566qq2581@gmail.com>
2025-10-08 17:45:10 +0800

    feat(server): add global logger

---
+++ apps/web/src/server/utils/logger/index.ts
@@ -0,0 +1,54 @@
+import path from "node:path";
+import { format, loggers, transports } from "winston";
+
+const {
+    combine,
+    label,
+    json,
+    timestamp,
+    errors,
+    colorize,
+    printf
+} = format;
+
+const customConsoleFormat = printf(({ level, message, label, timestamp, stack, ...meta }) => {
+    const args = meta[Symbol.for("splat")] as any[] || [];
+    const argString = args.map(arg => {
+        if (typeof arg === "object") {
+            const argCopy = structuredClone(arg);
+            return Object.entries(argCopy).map(([key, value]) => `${key}: ${value}`).join(", ");
+        }
+        return arg;
+    }).join(", ");
+
+    return `[${timestamp}] [${label}] [${level}] ${message} ${argString}`;
+});
+
+loggers.add("brigid", {
+    level: "info",
+    transports: [
+        new transports.Console({ 
+            format: combine(
+                errors({ stack: true }),
+                label({ label: "brigid-node" }),
+                timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+                customConsoleFormat,
+                colorize({ all: true })
+            )
+        }),
+        new transports.File({
+            filename: path.join(process.cwd(), "logs", "brigid-node.log"),
+            maxFiles: 3,
+            maxsize: 15 * 1024 * 1024,
+            tailable: true,
+            format: combine(
+                errors({ stack: true }),
+                label({ label: "brigid-node" }),
+                timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+                json()
+            )
+        })
+    ]
+});
+
+export const appLogger = loggers.get("brigid");
