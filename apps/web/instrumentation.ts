1de45006daa8f6a343437258e13c94329d6fb20a
chinlinlee <a5566qq2581@gmail.com>
2025-10-08 17:46:19 +0800

    refactor(server): initialize database in instrumentation

--- apps/web/instrumentation.ts
+++ apps/web/instrumentation.ts
@@ -1,5 +1,9 @@
 export async function register() {
     if (process.env.NEXT_RUNTIME === "nodejs") {
+        const { AppDataSource } = await import("@brigid/database/src/dataSource");
+        if (!AppDataSource.isInitialized) {
+            await AppDataSource.initialize();
+        }
         const raccoonDcm4cheBridge = await import("raccoon-dcm4che-bridge");
         raccoonDcm4cheBridge.raccoonDcm4cheJavaLoader({
             isPackagedElectron: true
