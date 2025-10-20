4743e3d1b23b3e98cf96182cd51881aa0bf90bb6
chinlinlee <a5566qq2581@gmail.com>
2025-10-20 15:51:00 +0800

    feat(database): update `modalitiesInStudy` in study json after series insertion

--- packages/database/src/subscribers/series.subscriber.ts
+++ packages/database/src/subscribers/series.subscriber.ts
@@ -1,4 +1,5 @@
-import { 
+import type { DicomTag } from "@brigid/types";
+import {
     type EntitySubscriberInterface,
     EventSubscriber,
     type InsertEvent,
@@ -15,8 +16,8 @@ export class SeriesSubscriber
     }
 
     async afterInsert(event: InsertEvent<SeriesEntity>): Promise<void> {
-        const studyEntityRepository = event.manager.getRepository(StudyEntity);
-        await studyEntityRepository.increment(
+        await event.manager.increment(
+            StudyEntity,
             {
                 studyInstanceUid: event.entity.studyInstanceUid,
                 workspaceId: event.entity.workspaceId,
@@ -24,5 +25,44 @@ export class SeriesSubscriber
             "numberOfStudyRelatedSeries",
             1,
         );
+
+        if (event.entity.modality) {
+            const parentStudy = await event.manager
+                .createQueryBuilder(StudyEntity, "study")
+                .where("study.studyInstanceUid = :studyInstanceUid", {
+                    studyInstanceUid: event.entity.studyInstanceUid,
+                })
+                .andWhere("study.workspaceId = :workspaceId", {
+                    workspaceId: event.entity.workspaceId,
+                })
+                .getOne();
+
+            if (parentStudy) {
+                const parentStudyJson = JSON.parse(
+                    parentStudy.json ?? "{}",
+                ) as DicomTag;
+                if (!("00080061" in parentStudyJson)) {
+                    parentStudyJson["00080061"] = {
+                        vr: "CS",
+                        Value: [],
+                    };
+                }
+
+                const modalitiesArray = parentStudyJson["00080061"].Value as string[];
+                if (!modalitiesArray.includes(event.entity.modality)) {
+                    modalitiesArray.push(event.entity.modality);
+                }
+
+                await event.manager.update(
+                    StudyEntity,
+                    {
+                        id: parentStudy.id,
+                    },
+                    {
+                        json: JSON.stringify(parentStudyJson),
+                    },
+                );
+            }
+        }
     }
 }
