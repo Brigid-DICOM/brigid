2556642e2f5e85602de415fcb6aed172f32d5413
chinlinlee <a5566qq2581@gmail.com>
2025-10-20 17:44:43 +0800

    build: update config for building project
    - Add `turbopackMinify` and `serverMinification` to avoid class minification for typeorm pakcage that cause runtime error

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -9,11 +9,12 @@ const nextConfig: NextConfig = {
             ".next/static/**/*",
         ]
     },
-    transpilePackages: ["@brigid/database", "@brigid/env", "@electric-sql/pglite"],
-    serverExternalPackages: ["raccoon-dcm4che-bridge", "hasha", "7zip-min"],
+    transpilePackages: ["@brigid/env", "@electric-sql/pglite"],
+    serverExternalPackages: ["raccoon-dcm4che-bridge", "hasha", "7zip-min", "typeorm", "@brigid/database"],
     experimental: {
-        serverSourceMaps: true
-    }
+        serverMinification: true,
+        turbopackMinify: false
+    },
 };
 
 export default nextConfig;
