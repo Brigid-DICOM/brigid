90670aaf679290dee886f7524ba0d0edc53bb6b3
chinlinlee <a5566qq2581@gmail.com>
2025-11-04 13:46:31 +0800

    refactor: extract multipart decode method

--- apps/web/src/server/utils/multipartMessage.ts
+++ apps/web/src/server/utils/multipartMessage.ts
@@ -6,21 +6,10 @@ import multiStream from "multistream";
 import type { Readable } from "stream";
 
 /**
- * Converts a Uint8Array to a String.
- * @param arr - that should be converted
- * @param offset - array offset in case only subset of array items should be extracted (default: 0)
- * @param limit - maximum number of array items that should be extracted (defaults to length of array)
- * @returns {String}
+ * @typedef {Object} MultipartEncodedData
+ * @property {ArrayBuffer} data The encoded Multipart Data
+ * @property {String} boundary The boundary used to divide pieces of the encoded data
  */
-function uint8ArrayToString(arr: Uint8Array, offset: number, limit: number) {
-    offset = offset || 0;
-    limit = limit || arr.length - offset;
-    let str = "";
-    for (let i = offset; i < offset + limit; i++) {
-        str += String.fromCharCode(arr[i]);
-    }
-    return str;
-}
 
 /**
  * Converts a String to a Uint8Array.
@@ -34,77 +23,6 @@ function stringToUint8Array(str: string) {
     }
     return arr;
 }
-
-/**
- * Identifies the boundary in a multipart/related message header.
- * @param header - message header
- * @returns {string} boundary
- */
-function identifyBoundary(header: string): string {
-    const parts = header.split("\r\n");
-
-    for (let i = 0; i < parts.length; i++) {
-        if (parts[i].substr(0, 2) === "--") {
-            return parts[i];
-        }
-    }
-
-    return "";
-}
-
-/**
- * Checks whether a given token is contained by a message at a given offset.
- * @param {Uint8Array} message message content
- * @param {Uint8Array} token substring that should be present
- * @param {Number} offset offset in message content from where search should start
- * @returns {Boolean} whether message contains token at offset
- */
-function containsToken(message: Uint8Array, token: Uint8Array, offset = 0): boolean {
-    if (offset + token.length > message.length) {
-        return false;
-    }
-
-    let index = offset;
-    for (let i = 0; i < token.length; i++) {
-        if (token[i] !== message[index++]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-/**
- * Finds a given token in a message at a given offset.
- * @param {Uint8Array} message message content
- * @param {Uint8Array} token substring that should be found
- * @param {Number} offset message body offset from where search should start
- * @returns {Boolean} whether message has a part at given offset or not
- */
-function findToken(message: Uint8Array, token: Uint8Array, offset = 0, maxSearchLength?: number): number {
-    let searchLength = message.length;
-    if (maxSearchLength) {
-        searchLength = Math.min(offset + maxSearchLength, message.length);
-    }
-
-    for (let i = offset; i < searchLength; i++) {
-        // If the first value of the message matches
-        // the first value of the token, check if
-        // this is the full token.
-        if (message[i] === token[0]) {
-            if (containsToken(message, token, i)) {
-                return i;
-            }
-        }
-    }
-
-    return -1;
-}
-
-/**
- * @typedef {Object} MultipartEncodedData
- * @property {ArrayBuffer} data The encoded Multipart Data
- * @property {String} boundary The boundary used to divide pieces of the encoded data
- */
 
 /**
  * Encode one or more DICOM datasets into a single body so it can be
@@ -196,74 +114,6 @@ function multipartEncodeByStream(
 }
 
 /**
- * Decode a Multipart encoded ArrayBuffer and return the components as an Array.
- *
- * @param {ArrayBuffer} response Data encoded as a 'multipart/related' message
- * @returns {Array} The content
- */
-function multipartDecode(response: ArrayBuffer): ArrayBuffer[] {
-    const message = new Uint8Array(response);
-
-    /* Set a maximum length to search for the header boundaries, otherwise
-       findToken can run for a long time
-    */
-    const maxSearchLength = 1000;
-
-    // First look for the multipart mime header
-    let separator = stringToUint8Array("\r\n\r\n");
-    let headerIndex = findToken(message, separator, 0, maxSearchLength);
-    if (headerIndex === -1) {
-        throw new Error("Response message has no multipart mime header");
-    }
-
-    const header = uint8ArrayToString(message, 0, headerIndex);
-    const boundaryString = identifyBoundary(header);
-    if (!boundaryString) {
-        throw new Error("Header of response message does not specify boundary");
-    }
-
-    const boundary = stringToUint8Array(boundaryString);
-    const components = [];
-
-    let offset = headerIndex + separator.length;
-
-    // Loop until we cannot find any more boundaries
-    let boundaryIndex: number | undefined;
-
-    while (boundaryIndex !== -1) {
-        // Search for the next boundary in the message, starting
-        // from the current offset position
-        boundaryIndex = findToken(message, boundary, offset);
-
-        // If no further boundaries are found, stop here.
-        if (boundaryIndex === -1) {
-            break;
-        }
-
-        // Extract data from response message, excluding "\r\n"
-        const spacingLength = 2;
-        const length = boundaryIndex - offset - spacingLength;
-        const data = response.slice(offset, offset + length);
-
-        // Add the data to the array of results
-        components.push(data);
-
-        // find the end of the boundary
-        const boundaryEnd = findToken(
-            message,
-            separator,
-            boundaryIndex + 1,
-            maxSearchLength
-        );
-        if (boundaryEnd === -1) break;
-        // Move the offset to the end of the identified boundary
-        offset = boundaryEnd + separator.length;
-    }
-
-    return components;
-}
-
-/**
  * Create a random GUID
  *
  * @return {string}
@@ -291,14 +141,8 @@ function guid() {
 }
 
 const message = {
-    containsToken: containsToken,
-    findToken: findToken,
-    identifyBoundary: identifyBoundary,
-    uint8ArrayToString: uint8ArrayToString,
-    stringToUint8Array: stringToUint8Array,
     multipartEncode: multipartEncode,
     multipartEncodeByStream: multipartEncodeByStream,
-    multipartDecode: multipartDecode,
     guid: guid
 };
 
