858b8ad8e81ad798f845d678945e1e71f74a8f90
chinlinlee <a5566qq2581@gmail.com>
2025-12-18 19:49:22 +0800

    feat(dimse): implement C-STORE SCP service
    - Added C-STORE SCP service to handle incoming DICOM file storage requests

---
+++ apps/web/src/server/dimse/cstoreScp.ts
@@ -0,0 +1,165 @@
+import path from "node:path";
+import { AppDataSource } from "@brigid/database";
+import { DimseConfigEntity } from "@brigid/database/src/entities/dimseConfig.entity";
+import fsE from "fs-extra";
+import { File } from "raccoon-dcm4che-bridge/src/wrapper/java/io/File";
+import type { Attributes } from "raccoon-dcm4che-bridge/src/wrapper/org/dcm4che3/data/Attributes";
+import { Tag } from "raccoon-dcm4che-bridge/src/wrapper/org/dcm4che3/data/Tag";
+import type { Association } from "raccoon-dcm4che-bridge/src/wrapper/org/dcm4che3/net/Association";
+import { createAssociationListenerProxy } from "raccoon-dcm4che-bridge/src/wrapper/org/dcm4che3/net/AssociationListener";
+import { createCStoreSCPInjectProxy } from "raccoon-dcm4che-bridge/src/wrapper/org/github/chinlinlee/dcm777/net/CStoreSCPInject";
+import { SimpleCStoreSCP } from "raccoon-dcm4che-bridge/src/wrapper/org/github/chinlinlee/dcm777/net/SimpleCStoreSCP";
+import tmp from "tmp";
+import { StowRsService } from "../services/stowRs.service";
+import type { MultipartFile } from "../types/file";
+import { appLogger } from "../utils/logger";
+
+const logger = appLogger.child({
+    module: "NativeCStoreScp",
+});
+
+let scpInstance: SimpleCStoreSCP | undefined;
+
+async function getContextKey(
+    association: Association,
+    requestAttr: Attributes,
+) {
+    const serialNo = await association?.getSerialNo();
+    const messageId = await requestAttr?.getInt(Tag.MessageID, -1);
+
+    return `serialNo-${serialNo}:messageId-${messageId}`;
+}
+
+const cStoreScpInjectProxy = createCStoreSCPInjectProxy(
+    {
+        // @ts-expect-error java bridge type mismatch
+        preDimseRQ: async (
+            association,
+            _presentationContext,
+            _dimse,
+            requestAttr,
+        ) => {
+            if (!association || !requestAttr) return;
+
+            const contextKey = await getContextKey(association, requestAttr);
+            const sourceAET = await association?.getRemoteAET();
+            const sourceHost = await association?.getRemoteHostName();
+
+            logger.info(
+                `Incoming DIMSE request from ${sourceAET}@${sourceHost}`,
+                {
+                    op: "C-STORE",
+                    contextKey,
+                },
+            );
+
+            const listener = createAssociationListenerProxy(
+                {
+                    onClose: () => {
+                        logger.info(
+                            `Association (${sourceAET}@${sourceHost}) closed`,
+                            {
+                                op: "C-STORE",
+                                contextKey,
+                            },
+                        );
+                    },
+                },
+                {
+                    keepAsDaemon: true,
+                },
+            );
+
+            await association?.addAssociationListener(listener);
+
+            return true;
+        },
+        postDimseRQ: async (
+            association,
+            presentationContext,
+            _dimse,
+            _requestAttr,
+            _data,
+            responseAttr,
+        ) => {
+            return await association?.tryWriteDimseRSP(
+                presentationContext,
+                responseAttr,
+            );
+        },
+        postStore: async (
+            association,
+            _presentationContext,
+            requestAttr,
+            _data,
+            _responseAttr,
+            file,
+        ) => {
+            if (!association || !requestAttr || !file) return;
+
+            const contextKey = await getContextKey(association, requestAttr);
+            const absPath = await file?.getAbsoluteFile();
+            const aeTitle = await association?.getCalledAET();
+            if (!aeTitle || !absPath) return;
+
+            const hitDimseConfig = await AppDataSource.manager.findOne(
+                DimseConfigEntity,
+                {
+                    where: {
+                        aeTitle,
+                    },
+                },
+            );
+
+            if (!hitDimseConfig) return;
+
+            const stowRsService = new StowRsService(hitDimseConfig.workspaceId);
+
+            // 轉換成給 stowRsService 使用的 MultipartFile
+            const multipartFile: MultipartFile = {
+                originalFilename: path.basename(absPath.toString()),
+                filename: absPath.toString(),
+                size: await fsE
+                    .stat(absPath.toString())
+                    .then((stat) => stat.size),
+                mediaType: "application/dicom",
+            };
+
+            try {
+                const { message } = await stowRsService.storeDicomFiles([
+                    multipartFile,
+                ]);
+
+                if (message["00081198"].Value.length > 0) {
+                    throw new Error(
+                        `Failed to store DICOM file, ${JSON.stringify(message["00081198"].Value)}`,
+                    );
+                }
+            } catch (error) {
+                logger.error("Failed to store DICOM file", error, {
+                    op: "C-STORE",
+                    contextKey,
+                });
+                throw error;
+            }
+        },
+    },
+    {
+        keepAsDaemon: true,
+    },
+);
+
+export function getScpInstance() {
+    if (scpInstance) {
+        return scpInstance;
+    }
+
+    const tmpDir = new File(tmp.dirSync().name);
+
+    const basicCStoreScp = new SimpleCStoreSCP(cStoreScpInjectProxy, tmpDir, [
+        "*",
+    ]);
+
+    scpInstance = basicCStoreScp;
+    return basicCStoreScp;
+}
--- apps/web/src/server/dimse/index.ts
+++ apps/web/src/server/dimse/index.ts
@@ -15,53 +15,62 @@ import { DicomServiceRegistry } from "raccoon-dcm4che-bridge/src/wrapper/org/dcm
 import { TransferCapability } from "raccoon-dcm4che-bridge/src/wrapper/org/dcm4che3/net/TransferCapability";
 import { TransferCapability$Role } from "raccoon-dcm4che-bridge/src/wrapper/org/dcm4che3/net/TransferCapability$Role";
 import { Common } from "raccoon-dcm4che-bridge/src/wrapper/org/github/chinlinlee/dcm777/common/Common";
+import { getScpInstance } from "./cstoreScp";
 
 interface DimseConfigInfo {
     aeTitle: string;
     workspaceId: string;
 }
 
-
 export class DimseApp {
     private device: Device;
     private applicationEntities: Map<string, ApplicationEntity> = new Map();
     private connection = new Connection();
 
-    constructor(private hostname: string, private port: number) {
+    constructor(
+        private hostname: string,
+        private port: number,
+    ) {
         this.device = new Device("brigid");
         this.device.addConnectionSync(this.connection);
     }
 
-    async start () {
+    async start() {
         this.configureLog();
         this.configureConnection();
         this.configureBindServer();
 
         await this.loadApplicationEntitiesFromDatabase();
-        
+
         const dicomServiceRegistry = await this.createDicomServiceRegistry();
         this.device.setDimseRQHandlerSync(dicomServiceRegistry);
 
         const Executors = importClass("java.util.concurrent.Executors");
         const executorService = await Executors.newCachedThreadPool();
-        const scheduledExecutorService = await Executors.newSingleThreadScheduledExecutor();
-        
+        const scheduledExecutorService =
+            await Executors.newSingleThreadScheduledExecutor();
+
         await this.device.setScheduledExecutor(scheduledExecutorService);
         await this.device.setExecutor(executorService);
         await this.device.bindConnections();
 
-        console.log(`DIMSE SCP started on ${env.DIMSE_HOSTNAME}:${env.DIMSE_PORT}`);
+        console.log(
+            `DIMSE SCP started on ${env.DIMSE_HOSTNAME}:${env.DIMSE_PORT}`,
+        );
     }
 
     private async loadApplicationEntitiesFromDatabase() {
-        const enabledConfigs = await AppDataSource.manager.find(DimseConfigEntity, {
-            where: { enabled: true }
-        });
+        const enabledConfigs = await AppDataSource.manager.find(
+            DimseConfigEntity,
+            {
+                where: { enabled: true },
+            },
+        );
 
         for (const config of enabledConfigs) {
             this.addApplicationEntityToDevice({
                 aeTitle: config.aeTitle,
-                workspaceId: config.workspaceId
+                workspaceId: config.workspaceId,
             });
         }
     }
@@ -70,7 +79,9 @@ export class DimseApp {
         const { aeTitle, workspaceId } = config;
 
         if (this.applicationEntities.has(aeTitle)) {
-            console.warn(`Application Entity ${aeTitle} already exists, skipping`);
+            console.warn(
+                `Application Entity ${aeTitle} already exists, skipping`,
+            );
             return this.applicationEntities.get(aeTitle) as ApplicationEntity;
         }
 
@@ -83,12 +94,18 @@ export class DimseApp {
         this.device.addApplicationEntitySync(ae);
         this.applicationEntities.set(aeTitle, ae);
 
-        console.log(`Added Application Entity: ${aeTitle} for workspace: ${workspaceId}`);
+        console.log(
+            `Added Application Entity: ${aeTitle} for workspace: ${workspaceId}`,
+        );
         return ae;
     }
 
-    addApplicationEntitiesToDevice(configs: DimseConfigInfo[]): ApplicationEntity[] {
-        return configs.map((config) => this.addApplicationEntityToDevice(config));
+    addApplicationEntitiesToDevice(
+        configs: DimseConfigInfo[],
+    ): ApplicationEntity[] {
+        return configs.map((config) =>
+            this.addApplicationEntityToDevice(config),
+        );
     }
 
     removeApplicationEntityFromDevice(aeTitle: string): boolean {
@@ -110,9 +127,12 @@ export class DimseApp {
     private async createDicomServiceRegistry() {
         const dicomServiceRegistry = new DicomServiceRegistry();
         await dicomServiceRegistry.addDicomService(new BasicCEchoSCP());
+
+        const nativeCStoreScp = getScpInstance();
+        await dicomServiceRegistry.addDicomService(nativeCStoreScp);
         return dicomServiceRegistry;
     }
-    
+
     private configureBindServer() {
         this.connection.setPortSync(this.port);
         this.connection.setHostnameSync(this.hostname);
@@ -123,46 +143,67 @@ export class DimseApp {
             null,
             "*",
             TransferCapability$Role.SCP,
-            ["*"]
+            ["*"],
         );
         tc.setQueryOptionsSync(EnumSet.noneOfSync(QueryOption.class as Class));
         ae.addTransferCapabilitySync(tc);
     }
 
     private configureConnection() {
-        this.connection.setReceivePDULengthSync(env.DIMSE_MAX_PDU_LEN_RCV as number);
-        this.connection.setSendPDULengthSync(env.DIMSE_MAX_PDU_LEN_SND as number);
+        this.connection.setReceivePDULengthSync(
+            env.DIMSE_MAX_PDU_LEN_RCV as number,
+        );
+        this.connection.setSendPDULengthSync(
+            env.DIMSE_MAX_PDU_LEN_SND as number,
+        );
 
         if (env.DIMSE_NOT_ASYNC) {
             this.connection.setMaxOpsInvokedSync(1);
             this.connection.setMaxOpsPerformedSync(1);
         } else {
-            this.connection.setMaxOpsInvokedSync(env.DIMSE_MAX_OPS_INVOKED as number);
-            this.connection.setMaxOpsPerformedSync(env.DIMSE_MAX_OPS_PERFORMED as number);
+            this.connection.setMaxOpsInvokedSync(
+                env.DIMSE_MAX_OPS_INVOKED as number,
+            );
+            this.connection.setMaxOpsPerformedSync(
+                env.DIMSE_MAX_OPS_PERFORMED as number,
+            );
         }
 
         this.connection.setPackPDVSync(!env.DIMSE_NOT_PACK_PDV);
-        this.connection.setConnectTimeoutSync(env.DIMSE_CONNECT_TIMEOUT as number);
-        this.connection.setRequestTimeoutSync(env.DIMSE_REQUEST_TIMEOUT as number);
-        this.connection.setAcceptTimeoutSync(env.DIMSE_ACCEPT_TIMEOUT as number);
-        this.connection.setReleaseTimeoutSync(env.DIMSE_RELEASE_TIMEOUT as number);
+        this.connection.setConnectTimeoutSync(
+            env.DIMSE_CONNECT_TIMEOUT as number,
+        );
+        this.connection.setRequestTimeoutSync(
+            env.DIMSE_REQUEST_TIMEOUT as number,
+        );
+        this.connection.setAcceptTimeoutSync(
+            env.DIMSE_ACCEPT_TIMEOUT as number,
+        );
+        this.connection.setReleaseTimeoutSync(
+            env.DIMSE_RELEASE_TIMEOUT as number,
+        );
         this.connection.setSendTimeoutSync(env.DIMSE_SEND_TIMEOUT as number);
         this.connection.setStoreTimeoutSync(env.DIMSE_STORE_TIMEOUT as number);
-        this.connection.setResponseTimeoutSync(env.DIMSE_RESPONSE_TIMEOUT as number);
+        this.connection.setResponseTimeoutSync(
+            env.DIMSE_RESPONSE_TIMEOUT as number,
+        );
         this.connection.setIdleTimeoutSync(env.DIMSE_IDLE_TIMEOUT as number);
-        this.connection.setSocketCloseDelaySync(env.DIMSE_SO_CLOSE_DELAY as number);
-        this.connection.setSendBufferSizeSync(env.DIMSE_SO_SND_BUFFER as number);
-        this.connection.setReceiveBufferSizeSync(env.DIMSE_SO_RCV_BUFFER as number);
+        this.connection.setSocketCloseDelaySync(
+            env.DIMSE_SO_CLOSE_DELAY as number,
+        );
+        this.connection.setSendBufferSizeSync(
+            env.DIMSE_SO_SND_BUFFER as number,
+        );
+        this.connection.setReceiveBufferSizeSync(
+            env.DIMSE_SO_RCV_BUFFER as number,
+        );
         this.connection.setTcpNoDelaySync(!!env.DIMSE_TCP_NO_DELAY);
     }
 
     private configureLog() {
         this.generateLogBackFile();
         Common.LoadLogConfigSync(
-            path.join(
-                process.cwd(),
-                "configs/logback.xml",
-            )
+            path.join(process.cwd(), "configs/logback.xml"),
         );
     }
 
