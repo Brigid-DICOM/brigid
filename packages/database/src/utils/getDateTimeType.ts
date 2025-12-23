e11239c3623f49c1e9b46f2130a017e33400600c
chinlinlee <a5566qq2581@gmail.com>
2025-12-23 19:23:03 +0800

    fix(database): `getDateTimeType` return `datetime` when type being sqlite

--- packages/database/src/utils/getDateTimeType.ts
+++ packages/database/src/utils/getDateTimeType.ts
@@ -1,5 +1,10 @@
+import env from "@brigid/env";
+import { parseDataSourceConfig } from "./parseDataSourceConfig";
+
+const dataSourceConfig = parseDataSourceConfig(env.TYPEORM_CONNECTION);
+
 export function getDateTimeType() {
-    if (process.env.NODE_ENV === "test") {
+    if (process.env.NODE_ENV === "test" || dataSourceConfig.type === "better-sqlite3") {
         return "datetime";
     }
 
