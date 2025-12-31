e451a70bcbd0abeea82d404c71499d742b1eeb82
chinlinlee <a5566qq2581@gmail.com>
2025-12-31 21:04:26 +0800

    fix(pre-build): correct example env path

--- apps/web/prebuild.ts
+++ apps/web/prebuild.ts
@@ -3,10 +3,10 @@ import path from "node:path";
 
 try {
     const envPath = path.join(process.cwd(), ".env");
-    const envExamplePath = path.join(process.cwd(), ".env.example");
+    const envExamplePath = path.join(process.cwd(), "env.example");
 
     if (!fs.existsSync(envPath)) {
-        console.warn("⚠️ .env file not found, copying from .env.example");
+        console.warn("⚠️ .env file not found, copying from env.example");
         fs.copyFileSync(envExamplePath, envPath);
     }
 } catch (error) {
